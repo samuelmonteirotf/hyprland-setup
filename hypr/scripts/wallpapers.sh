a3dfbdae6d8aed09ab3d51545efbdaf3246923af
samuelmonteirotf <samuel@monteirotf.com>
2025-01-27 17:36:34 -0300

    Atualização das configurações do Hyperland

--- hypr/scripts/wallpapers.sh
+++ hypr/scripts/wallpapers.sh
@@ -13,7 +13,7 @@ HYPRPAPER_CONF="$HOME/.config/hypr/hyprpaper.conf"
 echo " " > $HYPRPAPER_CONF
 
 #Mudar o conteudo hyprpapar.conf
-echo "preload = $WALLPAPAER" >> $HYPRPAPER_CONF
+echo "preload = $WALLPAPER" >> $HYPRPAPER_CONF
 echo "wallpaper = DP-2,$WALLPAPER" >> $HYPRPAPER_CONF
 echo "splash = false" >> HYPRPAPER_CONF
 
